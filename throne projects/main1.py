f8cb7656f6f06a96826536399485548b3636fdae
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-23 15:27:45 +0200

    throne codes
    these are codes written using throne ide

---
+++ throne projects/main1.py
@@ -0,0 +1,11 @@
+pi= 22.0/7.0
+def circle_area(radius):
+    area = radius*radius*pi
+    print("the area is : ", area )
+
+def circle_perimeter(radius):
+    perimeter = 2*radius *pi
+    print("the perimeter is :", perimeter)
+
+circle_area(5.5)
+circle_perimeter(5.5)
---
+++ throne projects/main3.py
@@ -0,0 +1,7 @@
+i = 0 ; sum =0
+list = [1,2,3,4,5,6,7,8,9]
+for num in list :
+   sum = sum + i
+   i = i + 1
+
+print("the sum of list is  : ", sum )
