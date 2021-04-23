6d832f2ef37ae47fb949d8f58ce173c4e56f190e
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-23 14:52:11 +0200

    zero_5
    this is the fifth code

---
+++ EL_Zero[ mastering python ]/zero_5.py
@@ -0,0 +1,68 @@
+name ="merna" ; age = 14
+print(name.isspace())
+
+print("="*40)
+print(5200>254)
+
+print("="*40)
+# true values
+print(bool("merna"))
+print(bool(52))
+print(bool({1 , 2 , 'e' }))
+print(bool((1 ,12  , 47)))
+print(bool("true"))
+print(bool(True))
+print(bool(1))
+
+print("_"*40)
+
+# false values
+print(bool(""))
+print(bool(''))
+print(bool({ }))
+print(bool([ ]))
+print(bool(False))
+print(bool(0))
+print(bool(()))
+
+print("_"*40)
+
+# boolean operators
+print(not name =='merna')
+print(not age == 15)
+print(name=='merna' and age ==14)
+
+
+
+# assignment operators
+x = 10 ; y = 40
+print(x)
+x = 20
+print(x)
+print(x)
+z = x*2 + y*4
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
