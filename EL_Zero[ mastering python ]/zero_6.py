c19cf5ec9d01d74a1af54cec02de856b4371d05d
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-23 14:52:36 +0200

    zeor_6
    this is the sixth code

---
+++ EL_Zero[ mastering python ]/zero_6.py
@@ -0,0 +1,27 @@
+print( "1000" * "Mabrook" )
+
+
+
+
+
+
+
+'''          # str()
+a = 10
+print("the value a is :", a)
+print(type(str(a)))
+print(type(a))
+a = str(a)
+print(type(a))'''
+
+
+
+
+"""b = str(a)
+print(b)
+c = float(a)
+print(c)
+print(b*20)
+print(c*20)
+print(a*20)"""
+
