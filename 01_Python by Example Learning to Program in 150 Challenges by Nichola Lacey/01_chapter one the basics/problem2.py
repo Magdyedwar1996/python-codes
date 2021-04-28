4deaf9aec808a66ebbc385e7a8f6a766ecc8cb09
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-28 18:21:44 +0200

    problem two

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/01_chapter one the basics/problem2.py
@@ -0,0 +1,9 @@
+"""
+002
+Ask for the user’s first name and then ask for
+their surname and display the output message
+Hello [First Name] [Surname]
+"""
+name = input ("Enter your name please:")
+surname = input("Enter your surname please :")
+print(f"Hello {name} {surname}")
