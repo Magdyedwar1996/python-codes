4e59327e740109003bca463e0fb684fd39f3146c
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-28 18:22:52 +0200

    problem seven

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/01_chapter one the basics/problem7.py
@@ -0,0 +1,10 @@
+"""
+007
+Ask the user for their name and their age. Add 1 to their age
+and display the output [Name] next birthday you
+will be [new age]
+"""
+
+name = input("Enter your name plz : ")
+age = int(input("Enter your age plz :"))
+print("your next birthday you will be :", age+1 )
