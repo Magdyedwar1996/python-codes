7d2e52ee24bc3616f725a00f8247b61eaa958490
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-28 19:01:56 +0200

    problem 8

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/01_chapter one the basics/problem10.py
@@ -0,0 +1,8 @@
+"""010
+There are 2,204 pounds in a kilogram. Ask the
+user to enter a weight in kilograms and convert it
+to pounds."""
+
+Kilograms = float(input("Enter the number of Kilograms : "))
+Number_Of_Pounds = Kilograms * 2.204
+print("There are : "+ str(Number_Of_Pounds) + " pounds ")
