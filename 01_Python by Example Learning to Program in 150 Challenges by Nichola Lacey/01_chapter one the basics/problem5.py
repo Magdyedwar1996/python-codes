16d780032f71cab50a717f12b9887f5302f121a5
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-28 18:22:32 +0200

    problem five

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/01_chapter one the basics/problem5.py
@@ -0,0 +1,16 @@
+"""
+005
+Ask the user to enter three
+numbers. Add together the first
+two numbers and then multiply
+this total by the third. Display the
+answer as The answer is
+[answer]
+"""
+number1 = float(input("Enter three numbers plz :"))
+number2 = float (input())
+number3 = float (input())
+
+sum = number1 + number2
+total = sum * number3
+print("The total is \n",total)
