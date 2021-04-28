e017061c12eb0385250efd60b2f750d395761d06
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-28 18:22:12 +0200

    problem four

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/01_chapter one the basics/problem4.py
@@ -0,0 +1,13 @@
+"""
+004
+Ask the user to enter
+two numbers. Add
+them together and
+display the answer as
+The total is
+[answer].
+"""
+number1 = float(input("Enter two numbers plz :"))
+number2 = float (input())
+sum = number1 + number2
+print("The total is \n",sum)
