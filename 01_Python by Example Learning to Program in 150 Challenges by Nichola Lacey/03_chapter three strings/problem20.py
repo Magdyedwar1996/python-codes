e9b624e91f3aaa3c7924308ca33ef1094455b417
magdy_edwar <magdyedwar1996@gmail.com>
2021-04-29 11:25:37 +0200

    problem 20

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/03_chapter three strings/problem20.py
@@ -0,0 +1,10 @@
+"""
+020
+Ask the user to enter
+their first name and
+then display the
+length of their name.
+"""
+while 1 :
+    name = input("Enter your name please : ")
+    print("Your name length is ",len(name))
