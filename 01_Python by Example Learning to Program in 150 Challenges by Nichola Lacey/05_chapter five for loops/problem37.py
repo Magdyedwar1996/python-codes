bb6c9a4b3c1cc090027b24ff4a59f0ce4e3d2c58
magdy_edwar <magdyedwar1996@gmail.com>
2021-05-07 00:16:01 +0200

    problem   37

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/05_chapter five for loops/problem37.py
@@ -0,0 +1,11 @@
+"""
+037
+Ask the user to enter their name and display each letter in
+their name on a separate line.
+"""
+i = "0"
+name = input("Enter your name please : ")
+name = list(name)
+#print(name)
+for i in list(name):
+   print(i)
