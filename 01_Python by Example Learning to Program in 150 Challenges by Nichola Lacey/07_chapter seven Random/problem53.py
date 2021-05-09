b3f8c5f7042178cb411e7058c6e86efa05558ec5
magdy_edwar <magdyedwar1996@gmail.com>
2021-05-09 19:06:47 +0200

    problem53 Random fruit

---
+++ 01_Python by Example Learning to Program in 150 Challenges by Nichola Lacey/07_chapter seven Random/problem53.py
@@ -0,0 +1,8 @@
+"""
+053
+Display a random fruit from a list of five fruits
+"""
+import random
+fruits = ["orange","Apple","banana","grape","strawberry"]
+fruit = random.choice(fruits)
+print(fruit)
