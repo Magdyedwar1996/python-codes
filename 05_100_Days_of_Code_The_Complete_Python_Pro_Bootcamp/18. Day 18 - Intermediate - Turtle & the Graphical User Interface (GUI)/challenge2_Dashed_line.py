4ad6a1d272794666d13537d39fa5ea4591b3fc0c
magdy_edwar <magdyedwar1996@gmail.com>
2021-05-13 20:34:41 +0200

    Drawing a Dashed Line with turtle

---
+++ 05_100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/18. Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/challenge2_Dashed_line.py
@@ -0,0 +1,14 @@
+from turtle import *
+Turtle2 = Turtle()
+for i in range(10):
+    Turtle2.forward(10)
+    Turtle2.penup()
+    Turtle2.forward(10)
+    Turtle2.pendown()
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
