58f3cce8154df9909542311ef93e271e30a29ff5
magdy_edwar <magdyedwar1996@gmail.com>
2021-05-13 20:32:15 +0200

    this is a simple way to draw a square

---
+++ 05_100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/18. Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/challenge1_square.py
@@ -0,0 +1,48 @@
+from turtle import *
+turtle1 = Turtle()
+
+# this is the first beginner way to draw a square
+"""# first side
+turtle1.forward(200)
+turtle1.lt(90)
+
+# second side
+turtle1.forward(100)
+turtle1.lt(90)
+
+# third side
+turtle1.forward(200)
+turtle1.lt(90)
+
+# fourth side
+turtle1.forward(100)
+turtle1.lt(90)
+"""
+
+# this is to draw a square
+"""
+for i in range(4):
+    turtle1.forward(300)
+    turtle1.left(90)
+"""
+# this is to draw a triangle
+"""
+for i in range(3):
+    turtle1.forward(100)
+    turtle1.left(120)
+"""
+
+# this function is to draw any geometric shape
+def DrawShap(t,n,length):
+    for i in range(n):
+        t.forward(length)
+        t.left(360 / n)
+
+# this line to colour the turtle and the background
+turtle1.color("green" , "blue")
+
+turtle1.begin_fill()   # to begin colouring the background
+DrawShap(turtle1,4, 150)    # to
+turtle1.end_fill()     # to end colouring the background
+done()
+#(360/5)
