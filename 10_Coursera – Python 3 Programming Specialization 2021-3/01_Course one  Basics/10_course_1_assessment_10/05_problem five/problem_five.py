bf049ee1a82b23c287a49b1b40a3b5d840d5338c
magdy_edwar <magdyedwar1996@gmail.com>
2021-07-10 13:20:23 +0200

    problem 5

---
+++ 10_Coursera – Python 3 Programming Specialization 2021-3/01_Course one  Basics/10_course_1_assessment_10/05_problem five/problem_five.py
@@ -0,0 +1,11 @@
+str1 = "I love python"
+#magdy = list(str1)
+chars = []
+for i in range(len(str1)):
+    #if str1[i] == " ":
+     #   continue
+    chars.append(str1[i])
+
+print(chars)
+# HINT: what's the accumulator? That should go here.
+
