3670108b57bceb9fdf6aa6ceba6ab88100d9917c
magdy_edwar <magdyedwar1996@gmail.com>
2021-07-10 11:16:47 +0200

    problem three to count the number of words that have 'w' character

---
+++ 10_Coursera – Python 3 Programming Specialization 2021-3/01_Course one  Basics/07_course_1_assessment_7/03_problem 3/03_the problem_3_solution.py
@@ -0,0 +1,8 @@
+items = ["whirring", "wow!", "calendar", "wry", "glass", "", "llama","tumultuous","owing"]
+acc_num = 0
+for i in range(len(items)):
+    if 'w' in items[i]:
+        acc_num += 1
+
+
+#print(acc_num)
