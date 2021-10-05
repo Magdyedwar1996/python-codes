11f7da3ff7ecda772fe81925e738b0b88b0c32d0
Magdy Edwar <magdiedwar47@gmail.com>
2021-10-05 22:41:20 +0200

    this program is to show simply how coffee machine work using simple functions

---
+++ Coffee Machine/main.py
@@ -0,0 +1,24 @@
+"""from menu import MENU, resources
+
+
+def get_customer_order():
+    order = input("What would you like? (espresso/latte/cappuccino):")
+
+
+def turn_coffee_machine_off():
+    this function to turn off the machine by
+    the maintainers of the coffee machine
+"""
+"""import pandas as pd
+
+dic_movie = {
+    "movie":["Avengers: Endgame", "Avatar", "Titanic", "Star Wars: The Force Awakens", "Avengers: Infinity War"],
+    "collections":[2.796, 2.789, 2.187, 2.068, 2.048],
+    "release_yr":[2019, 2009, 1997, 2015, 2018]}
+
+movie_df = pd.DataFrame(dic_movie)
+print(movie_df)"""
+import matplotlib.pyplot as plt
+import numpy as np
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
